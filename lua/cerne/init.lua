fe6b5a22854d6523bc672008bcb44b152d0990a4
Miguel Cerne <miguelbadanycerne.20231@poli.ufrj.br>
2024-02-24 14:29:39 -0700

    feat: Kickstart based config, added some personal configs and harpoon

---
+++ lua/cerne/init.lua
@@ -0,0 +1,2 @@
+print("Hello World")
+require("cerne.mappings")
---
+++ lua/cerne/mappings.lua
@@ -0,0 +1,4 @@
+print("Hello World from Mappings")
+
+vim.keymap.set('n', '<leader>yf', 'ggVG"+y')
+vim.keymap.set('n', '<leader>pv', ':Ex')

60591b69f1c439fd711b1354b6c61217c44940ea
Cerne17 <miguelbadanycerne.20231@poli.ufrj.br>
2024-03-10 10:55:03 -0300

    chore(plugin): Added descriptions to all harpoon maps

--- lua/harpoon-setup.lua
+++ lua/harpoon-setup.lua
@@ -4,17 +4,18 @@ local harpoon = require("harpoon")
 harpoon:setup({})
 -- REQUIRED
 
-vim.keymap.set("n", "<leader>m", function() harpoon:list():append() end)
-vim.keymap.set("n", "<leader>ht", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
+vim.keymap.set("n", "<leader>m", function() harpoon:list():append() end, { desc = "Add current file to harpoon list" })
+vim.keymap.set("n", "<leader>ht", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end,
+  { desc = "Toggle harpoon list (without telescope)" })
 
-vim.keymap.set("n", "<C-h><C-h>", function() harpoon:list():select(1) end)
-vim.keymap.set("n", "<C-h><C-j>", function() harpoon:list():select(2) end)
-vim.keymap.set("n", "<C-h><C-k>", function() harpoon:list():select(3) end)
-vim.keymap.set("n", "<C-h><C-l>", function() harpoon:list():select(4) end)
+vim.keymap.set("n", "<C-h><C-h>", function() harpoon:list():select(1) end, { desc = "[H]arpoon Fisrt File" })
+vim.keymap.set("n", "<C-h><C-j>", function() harpoon:list():select(2) end, { desc = "[H]arpoon Second File" })
+vim.keymap.set("n", "<C-h><C-k>", function() harpoon:list():select(3) end, { desc = "[H]arpoon Third File" })
+vim.keymap.set("n", "<C-h><C-l>", function() harpoon:list():select(4) end, { desc = "[H]arpoon Fourth File" })
 
 -- Toggle previous & next buffers stored within Harpoon list
-vim.keymap.set("n", "<C-S-P>", function() harpoon:list():prev() end)
-vim.keymap.set("n", "<C-S-N>", function() harpoon:list():next() end)
+vim.keymap.set("n", "<C-S-P>", function() harpoon:list():prev() end, { desc = "[H]arpoon Next File" })
+vim.keymap.set("n", "<C-S-N>", function() harpoon:list():next() end, { desc = "[H]arpoon Previous File" })
 
 -- basic telescope configuration
 local conf = require("telescope.config").values

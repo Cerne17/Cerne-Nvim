629a59af044170d762ed5642dc0c54809b0e9e46
Cerne17 <miguelbadanycerne.20231@poli.ufrj.br>
2024-03-21 16:13:12 -0300

    feat(oil): updated the oil's config to show hidden files/folders by default

--- lua/oil-setup.lua
+++ lua/oil-setup.lua
@@ -1,2 +1,15 @@
-require('oil').setup()
+require('oil').setup({
+  view_options = {
+    -- Show files and directories that start with "."
+    show_hidden = true,
+    -- This function defines what is considered a "hidden" file
+    is_hidden_file = function(name, bufnr)
+      return vim.startswith(name, ".")
+    end,
+    -- This function defines what will never be shown, even when `show_hidden` is set
+    is_always_hidden = function(name, bufnr)
+      return false
+    end,
+  }
+})
 vim.keymap.set("n", "<leader>pv", "<CMD>Oil<CR>", { desc = "Open parent directory" })

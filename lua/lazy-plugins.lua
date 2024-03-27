33ff211a49dbf1fb56ef97d1672791dfc5e62d41
Cerne17 <miguelbadanycerne.20231@poli.ufrj.br>
2024-03-27 08:59:46 -0300

    chore: revert last commit

--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -21,22 +21,13 @@ require('lazy').setup({
     'neovim/nvim-lspconfig',
     dependencies = {
       -- Automatically install LSPs to stdpath for neovim
-      { 'williamboman/mason.nvim', config = true },
+      { 'williamboman/mason.nvim', config = true, },
       'williamboman/mason-lspconfig.nvim',
 
       -- Useful status updates for LSP
       -- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
       {
-        'j-hui/fidget.nvim',
-        opts = {
-          ensure_installed = {
-            "black",
-            "pyright",
-            "rust_analyzer",
-            "tsserver",
-            "sumneko_lua",
-          }
-        }
+        'j-hui/fidget.nvim', opts = {}
       },
 
       -- Additional lua configuration, makes nvim stuff amazing!

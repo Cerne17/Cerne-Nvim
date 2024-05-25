57cae0f3b426136035a1abd7b3da079d267d56f0
Cerne <miguelbadanycerne.20231@poli.ufrj.br>
2024-05-25 10:39:19 -0300

    chore(jester): added jester to the init.lua

--- init.lua
+++ init.lua
@@ -55,6 +55,10 @@ require 'oil-setup'
 --[[ Configure Undotree ]]
 -- (Version Control)
 require 'undotree-setup'
+
+-- [[ Configure Jester ]]
+-- (Testing Automation)
+require 'jester-setup'
 --
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et

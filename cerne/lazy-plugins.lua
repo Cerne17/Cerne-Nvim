0ebcc59b84c63ba73c5ace98899f27aca7c9c5ed
Cerne17 <miguelbadanycerne.20231@poli.ufrj.br>
2024-09-11 23:32:57 -0300

    feat: continued installing the basics

---
+++ cerne/lazy-plugins.lua
@@ -0,0 +1,58 @@
+require('lazy').setup({
+
+  {
+    -- LSP Configuration & Plugins
+    'neovim/nvim-lspconfig',
+    dependencies = {
+      -- Automatically install LSPs to stdpath for neovim
+      { 'williamboman/mason.nvim', config = true, },
+      'williamboman/mason-lspconfig.nvim',
+
+      -- Useful status updates for LSP
+      -- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
+      {
+        'j-hui/fidget.nvim', opts = {}
+      },
+
+      -- Additional lua configuration, makes nvim stuff amazing!
+      'folke/neodev.nvim',
+    },
+  },
+
+  {
+    -- Highlight, edit, and navigate code
+    'nvim-treesitter/nvim-treesitter',
+    dependencies = {
+      'nvim-treesitter/nvim-treesitter-textobjects',
+    },
+    build = ':TSUpdate',
+  },
+  -- NOTE: This is where your plugins related to LSP can be installed.
+  --  The configuration is done below. Search for lspconfig to find it below.
+  {
+    -- LSP Configuration & Plugins
+    'neovim/nvim-lspconfig',
+    dependencies = {
+      -- Automatically install LSPs to stdpath for neovim
+      { 'williamboman/mason.nvim', config = true, },
+      'williamboman/mason-lspconfig.nvim',
+
+      -- Useful status updates for LSP
+      -- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
+      {
+        'j-hui/fidget.nvim', opts = {}
+      },
+
+      -- Additional lua configuration, makes nvim stuff amazing!
+      'folke/neodev.nvim',
+    },
+  },
+  -- Primeagen's Colorscheme
+  { 
+    "rose-pine/neovim", name = "rose-pine"
+  },
+  -- Useful plugin to show you pending keybinds.
+  {
+    'folke/which-key.nvim',  opts = {}
+  },
+})
--- init.lua
+++ init.lua
@@ -1,1 +1,18 @@
-require ("cerne.lazy")
+-- [ Sets the leader key ]
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
+-- [ Installs Lazy Package Manager ]
+require("cerne.lazy-pm")
+
+-- [ Sets the default vim options ]
+require("cerne.options")
+
+-- [ Requires the basic plugins installations ]
+require("cerne.lazy-plugins")
+
+-- [ Requires the Rose Pine Colors Config ]
+require("cerne.plugins.rose-setup")
+
+-- [ Requires General vim's keymaps ]
+require("cerne.keymaps")

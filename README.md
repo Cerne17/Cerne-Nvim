ff588c4c5d52588465326993b295092d88cb3f49
Cerne17 <miguelbadanycerne.20231@poli.ufrj.br>
2024-09-13 15:57:12 -0300

    docs: updated the TOC

--- README.md
+++ README.md
@@ -8,7 +8,14 @@
 │       └── spec2.lua
 └── init.lua
 # 2. TOC
-|  Index  |  Plugin Name  |  Plugin Description  |  Plugin Link  |
-|---------|---------------|----------------------|---------------|
-|    1    | Treesitter    | Enhances Highlighting | https://github.com/nvim-treesitter/nvim-treesitter |
-|    2    | Mason         | Easier Plugins Installation | https://github.com/williamboman/mason.nvim |
+|  Index  |  Plugin Name  |  Plugin Category  |  Plugin Description  |  Plugin Link  |
+|---------|---------------|-------------------|----------------------|---------------|
+|    1    | Treesitter    |  Highlighting     | Enhances Highlighting                | https://github.com/nvim-treesitter/nvim-treesitter |
+|    2    | Mason         | Plugin Management | Easier Plugins Installation          | https://github.com/williamboman/mason.nvim |
+|    3    | Rose-Pine     | Colorscheme       | -                                    | https://github.com/rose-pine/neovim |
+|    4    | fidget        | LSP               | Useful status updates for LSP        | https://github.com/j-hui/fidget.nvim |
+|    5    | which-key     | Documentation     | Creates a Bar with all keymappings   | https://github.com/folke/which-key.nvim |
+|    6    | cmp           | LSP               | LSP Autocompletion                   | https://github.com/hrsh7th/cmp-nvim-lsp |
+|    7    | telescope     | Fuzzy Finder      | Searches Files on the File Tree      | https://github.com/nvim-telescope/telescope.nvim |
+|    8    | harpoon       | Tabs Manager      | Easily work with multiple files      |  |
+|    9    | lsp-colors    | Highlighting      | Uses LSP to customize the highlight  | https://github.com/folke/lsp-colors.nvim |
--- cerne/lazy-plugins.lua
+++ cerne/lazy-plugins.lua
@@ -1,6 +1,13 @@
 require('lazy').setup({
 
   {
+    -- Git Configuration Related
+    'tpope/vim-fugitive',
+    'tpope/vim-rhubarb',
+
+    -- Tabstop and shiftwidth Adjustment
+    'tpope/vim-sleuth',
+
     -- LSP Configuration & Plugins
     'neovim/nvim-lspconfig',
     dependencies = {
@@ -19,14 +26,6 @@ require('lazy').setup({
     },
   },
 
-  {
-    -- Highlight, edit, and navigate code
-    'nvim-treesitter/nvim-treesitter',
-    dependencies = {
-      'nvim-treesitter/nvim-treesitter-textobjects',
-    },
-    build = ':TSUpdate',
-  },
   -- NOTE: This is where your plugins related to LSP can be installed.
   --  The configuration is done below. Search for lspconfig to find it below.
   {
@@ -47,12 +46,183 @@ require('lazy').setup({
       'folke/neodev.nvim',
     },
   },
+
+  {
+    -- Autocompletion
+    'hrsh7th/nvim-cmp',
+    dependencies = {
+      -- Snippet Engine & its associated nvim-cmp source
+      {
+        'L3MON4D3/LuaSnip',
+        build = (function()
+          -- Build Step is needed for regex support in snippets
+          -- This step is not supported in many windows environments
+          -- Remove the below condition to re-enable on windows
+          if vim.fn.has 'win32' == 1 then
+            return
+          end
+          return 'make install_jsregexp'
+        end)(),
+      },
+      'saadparwaiz1/cmp_luasnip',
+
+      -- Adds LSP completion capabilities
+      'hrsh7th/cmp-nvim-lsp',
+      'hrsh7th/cmp-path',
+
+      -- Adds a number of user-friendly snippets
+      'rafamadriz/friendly-snippets',
+    },
+  },
+
+  {
+    -- Highlight, edit, and navigate code
+    'nvim-treesitter/nvim-treesitter',
+    dependencies = {
+      'nvim-treesitter/nvim-treesitter-textobjects',
+    },
+    build = ':TSUpdate',
+  },
+
+-- Fuzzy Finder (files, lsp, etc)
+  {
+    'nvim-telescope/telescope.nvim',
+    branch = '0.1.x',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      -- Fuzzy Finder Algorithm which requires local dependencies to be built.
+      -- Only load if `make` is available. Make sure you have the system
+      -- requirements installed.
+      {
+        'nvim-telescope/telescope-fzf-native.nvim',
+        -- NOTE: If you are having trouble with this installation,
+        --       refer to the README for telescope-fzf-native for more instructions.
+        build = 'make',
+        cond = function()
+          return vim.fn.executable 'make' == 1
+        end,
+      },
+    },
+  },
+
+  {
+    -- Adds git related signs to the gutter, as well as utilities for managing changes
+    'lewis6991/gitsigns.nvim',
+    opts = {
+      -- See `:help gitsigns.txt`
+      signs = {
+        add = { text = '+' },
+        change = { text = '~' },
+        delete = { text = '_' },
+        topdelete = { text = '‾' },
+        changedelete = { text = '~' },
+      },
+      on_attach = function(bufnr)
+        local gs = package.loaded.gitsigns
+
+        local function map(mode, l, r, opts)
+          opts = opts or {}
+          opts.buffer = bufnr
+          vim.keymap.set(mode, l, r, opts)
+        end
+
+        -- Navigation
+        map({ 'n', 'v' }, ']c', function()
+          if vim.wo.diff then
+            return ']c'
+          end
+          vim.schedule(function()
+            gs.next_hunk()
+          end)
+          return '<Ignore>'
+        end, { expr = true, desc = 'Jump to next hunk' })
+
+        map({ 'n', 'v' }, '[c', function()
+          if vim.wo.diff then
+            return '[c'
+          end
+          vim.schedule(function()
+            gs.prev_hunk()
+          end)
+          return '<Ignore>'
+        end, { expr = true, desc = 'Jump to previous hunk' })
+
+        -- Actions
+        -- visual mode
+        map('v', '<leader>hs', function()
+          gs.stage_hunk { vim.fn.line '.', vim.fn.line 'v' }
+        end, { desc = 'stage git hunk' })
+        map('v', '<leader>hr', function()
+          gs.reset_hunk { vim.fn.line '.', vim.fn.line 'v' }
+        end, { desc = 'reset git hunk' })
+        -- normal mode
+        map('n', '<leader>hs', gs.stage_hunk, { desc = 'git stage hunk' })
+        map('n', '<leader>hr', gs.reset_hunk, { desc = 'git reset hunk' })
+        map('n', '<leader>hS', gs.stage_buffer, { desc = 'git Stage buffer' })
+        map('n', '<leader>hu', gs.undo_stage_hunk, { desc = 'undo stage hunk' })
+        map('n', '<leader>hR', gs.reset_buffer, { desc = 'git Reset buffer' })
+        map('n', '<leader>hp', gs.preview_hunk, { desc = 'preview git hunk' })
+        map('n', '<leader>hb', function()
+          gs.blame_line { full = false }
+        end, { desc = 'git blame line' })
+        map('n', '<leader>hd', gs.diffthis, { desc = 'git diff against index' })
+        map('n', '<leader>hD', function()
+          gs.diffthis '~'
+        end, { desc = 'git diff against last commit' })
+
+        -- Toggles
+        map('n', '<leader>tb', gs.toggle_current_line_blame, { desc = 'toggle git blame line' })
+        map('n', '<leader>td', gs.toggle_deleted, { desc = 'toggle git show deleted' })
+
+        -- Text object
+        map({ 'o', 'x' }, 'ih', ':<C-U>Gitsigns select_hunk<CR>', { desc = 'select git hunk' })
+      end,
+    },
+  },
+
+{
+  -- Set lualine as statusline
+  'nvim-lualine/lualine.nvim',
+  -- See `:help lualine.txt`
+  opts = {
+    options = {
+      icons_enabled = true,
+      theme = 'auto',
+      component_separators = '|',
+      section_separators = '',
+    },
+  },
+},
+
+  {
+    'ThePrimeagen/harpoon',
+    branch = "harpoon2",
+    dependencies = { "nvim-lua/plenary.nvim" }
+  },
+
   -- Primeagen's Colorscheme
-  { 
-    "rose-pine/neovim", name = "rose-pine"
+  {
+    "rose-pine/neovim", name = "rose-pine",
+    lazy = true
+  },
+
+  -- SciFi Colorscheme
+  {
+    "scottmckendry/cyberdream.nvim",
+    lazy = false,
+    priority = 1000,
+  },
+
+  {
+    "folke/lsp-colors.nvim",
+    lazy = true
   },
   -- Useful plugin to show you pending keybinds.
   {
     'folke/which-key.nvim',  opts = {}
+  },
+
+  {
+    'mzlogin/vim-markdown-toc'
   },
 })

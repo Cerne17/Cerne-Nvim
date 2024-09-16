ea2b4be60df44d5613adf41aced3d96f5b769d21
Cerne <miguelbadanycerne.20231@poli.ufrj.br>
2024-09-16 20:36:05 -0300

    feat: new plugin - alpha.nvim - prettier menu

--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -258,6 +258,22 @@ require('lazy').setup({
     'github/copilot.vim'
   },
 
+  -- Generates a Prettier Neovim Menu Page
+    {
+    "goolord/alpha-nvim",
+    -- dependencies = { 'echasnovski/mini.icons' },
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    config = function()
+      local startify = require("alpha.themes.startify")
+      -- available: devicons, mini, default is mini
+      -- if provider not loaded and enabled is true, it will try to use another provider
+      startify.file_icons.provider = "devicons"
+      require("alpha").setup(
+        startify.config
+      )
+    end,
+  },
+
   -- Automatically generates and updates the .md TOC's
   {
     'mzlogin/vim-markdown-toc'

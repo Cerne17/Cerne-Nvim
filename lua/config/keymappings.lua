c53807335ff357853aa7c1cdaf8a3a89fad3e27c
Cerne17 <miguelcerne14@gmail.com>
2025-01-23 23:51:17 -0300

    feat(lazygit|keymapping): added the lazygit mapping

--- lua/config/keymappings.lua
+++ lua/config/keymappings.lua
@@ -4,11 +4,13 @@ local wk = require("which-key")
 local telescope = require("telescope.builtin")
 -- [[ END - Importing the plugins to set the mappings ]]
 
--- [[ Telescope ]]
 wk.add({
-	{"<leader>f", group = "find (telescope)"}, -- creates the grouping
+	-- [[ Telescope ]]
+	{ "<leader>f", group = "find (telescope)" }, -- creates the grouping
 	{ "<leader>ff", telescope.find_files, desc = "Find File", mode = "n" },
 	{ "<leader>fb", telescope.buffers, desc = "Find in Buffers", mode = "n" },
 	{ "<leader>fg", telescope.live_grep, desc = "Find with Live Grep", mode = "n" },
 	{ "<leader>fc", telescope.colorscheme, desc = "Find Colorscheme", mode = "n" },
+	{ "<leader>g", group = "git" }, -- creates the grouping
+	{ "<leader>gg", "<cmd>LazyGit<CR>", desc = "Opens LazyGit", mode = "n" },
 })

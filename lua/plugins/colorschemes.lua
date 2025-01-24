0da9f5a5f1fb695ccf3b76cd334ad29973e6462e
Cerne17 <miguelcerne14@gmail.com>
2025-01-24 11:05:17 -0300

    feat(colorschemes): added a new colorscheme

---
+++ lua/plugins/colorschemes.lua
@@ -0,0 +1,18 @@
+return {
+	{
+		"folke/tokyonight.nvim",
+		lazy = false,
+		priority = 1000,
+		opts = {
+			transparent = true,
+		},
+	},
+	{
+		"scottmckendry/cyberdream.nvim",
+		lazy = false,
+		priority = 1000,
+		opts = {
+			transparent = true,
+		},
+	},
+}

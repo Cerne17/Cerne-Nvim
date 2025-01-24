ecfd15350fcd954d1fe339ee3ba92877d57882d2
Cerne17 <miguelcerne14@gmail.com>
2025-01-23 23:34:42 -0300

    fix(telescope|which-key): fixed the mappings' syntax

--- lua/config/keymappings.lua
+++ lua/config/keymappings.lua
@@ -1,10 +1,14 @@
 local wk = require("which-key")
 
+-- [[ START - Importing the plugins to set the mappings ]]
+local telescope = require("telescope.builtin")
+-- [[ END - Importing the plugins to set the mappings ]]
+
 -- [[ Telescope ]]
 wk.add({
 	{"<leader>f", group = "find (telescope)"}, -- creates the grouping
-	{ "<leader>ff", "<cmd>Telescope find_file<CR>", desc = "Find File", mode = "n" },
-	{ "<leader>fb", "<cmd>Telescope buffers<CR>", desc = "Find in Buffers", mode = "n" },
-	{ "<leader>fg", "<cmd>Telescope live_grep<CR>", desc = "Find with Live Grep", mode = "n" },
-	{ "<leader>fc", "<cmd>Telescope colorscheme<CR>", desc = "Find Colorscheme", mode = "n" },
+	{ "<leader>ff", telescope.find_files, desc = "Find File", mode = "n" },
+	{ "<leader>fb", telescope.buffers, desc = "Find in Buffers", mode = "n" },
+	{ "<leader>fg", telescope.live_grep, desc = "Find with Live Grep", mode = "n" },
+	{ "<leader>fc", telescope.colorscheme, desc = "Find Colorscheme", mode = "n" },
 })
